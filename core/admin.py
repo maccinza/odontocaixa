ab5395d9b2fabec0cbde215f018cbaccfe5631e7
maccinza <maccinza@gmail.com>
2015-04-15 20:20:21 -0300

    Adicionada busca, adicionados filtros e removido branding da view de changelist

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,21 @@ from core.models import FormaPagamento, Paciente, Atendimento, Despesa
 class AtendimentoAdmin(admin.ModelAdmin):
     filter_horizontal = ['pacientes']
 
+    search_fields = ['pacientes__nome', 'pacientes__sobrenome', 'descricao']
+    list_filter = ['data', 'forma_pagamento', 'recebido']
+
+
+class DespesaAdmin(admin.ModelAdmin):
+
+    search_fields = ['descricao']
+    list_filter = ['categoria']
+
+class PacienteAdmin(admin.ModelAdmin):
+
+    search_fields = ['nome', 'sobrenome', 'email']
+
+
 admin.site.register(FormaPagamento)
-admin.site.register(Paciente)
+admin.site.register(Paciente, PacienteAdmin)
 admin.site.register(Atendimento, AtendimentoAdmin)
-admin.site.register(Despesa)
+admin.site.register(Despesa, DespesaAdmin)
